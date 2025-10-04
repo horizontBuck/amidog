7866256ccc2ce9624e6e9e68641b95a4f501246c
bunsen <bunsen@bunsen.bunsen>
2025-10-04 18:31:22 -0500

    menu correct

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrls: ['./header.scss']
 })
 export class Header {
+  isMenuOpen = false;
   constructor(private router: Router) {}
 
   navigateTo(path: string) {
     this.router.navigate([path]);
   }
+goTo(path: string) {
+  this.router.navigate([path]);
+this.toggleMenu();
+  this.closeMobileMenu();
+
+}
+toggleMenu() {
+  this.isMenuOpen = !this.isMenuOpen;
+  const mobileNav = document.getElementById('mobile-nav');
+  if (mobileNav) {
+    mobileNav.style.display = this.isMenuOpen ? 'block' : 'none';  // Mostrar u ocultar el menú
+  }
+}
+
+closeMobileMenu() {
+  this.isMenuOpen = false; // Actualizamos el estado del menú
+  const mobileNav = document.getElementById('mobile-nav');
+  if (mobileNav) {
+    mobileNav.style.display = 'none';  // Ocultar el menú
+  }
+}
+
+
+
+
 }
